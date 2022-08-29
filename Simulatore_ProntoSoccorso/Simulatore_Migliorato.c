e3df6d858b520940e6f43a70ddaef1b7d3bc8657
kobero98 <matteo.fede@live.it>
2022-08-29 13:07:45 +0200

    grafici verifica

--- Simulatore_ProntoSoccorso/Simulatore_Migliorato.c
+++ Simulatore_ProntoSoccorso/Simulatore_Migliorato.c
@@ -381,7 +381,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                     }
 
                     /*people in red code may die while waiting (if they don't get served immediately)... :( */
-                    /*else{
+                    else{
                         SelectStream(7);
                         p = Uniform(0,100);
                         int x = redNumber-SERVERSRED;
@@ -391,7 +391,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                             redNumber--;
                             currentJob[1]++;
                         }
-                    }*/
+                    }
                     break;
 
                 case yellow:
@@ -414,7 +414,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                 modifyServerDataColor(&trauma[index], t.current+getService(traumaParams), 1, orange); 
                                 t.traumaCompletion=NextEvent(trauma, SERVERSTRAUMA);
                             }
-                            /*else{
+                            else{
                                 SelectStream(9);
                                 p = Uniform(0,100);
                                 int x = traumaOrangeNumber-traumaInServiceOrange;
@@ -424,7 +424,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                     else decessi[currentBatch[2]]++;
                                     currentJob[2]++;
                                 }
-                            }*/
+                            }
                         }
                         else if(p<51.4){         
                             medicalOrangeNumber++;
@@ -438,7 +438,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                 modifyServerDataColor(&medical[index], t.current+getService(medicalParams), 1, orange); 
                                 t.medicalCompletion=NextEvent(medical, SERVERSMEDICAL);
                             }
-                            /*else{
+                            else{
                                 SelectStream(9);
                                 p = Uniform(0,100);
                                 int x = medicalOrangeNumber-medicalInServiceOrange;
@@ -448,7 +448,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                     else decessi[currentBatch[4]]++;
                                     currentJob[4]++;
                                 }
-                            }*/
+                            }
                         }
                         else{    
                             minorOrangeNumber++; 
@@ -462,7 +462,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                 modifyServerDataColor(&minor[index], t.current+getService(minorParams), 1, orange); 
                                 t.minorCompletion=NextEvent(minor, SERVERSMINOR);
                             }
-                            /*else{
+                            else{
                                 SelectStream(9);
                                 p = Uniform(0,100);
                                 int x = minorOrangeNumber-minorInServiceOrange;
@@ -472,7 +472,7 @@ int simulatore2(output matrix[][15],double decessi[], int iteration, int finite,
                                     else decessi[currentBatch[3]]++;
                                     currentJob[3]++;
                                 }
-                            }*/
+                            }
                         }
                     }
                     else{
